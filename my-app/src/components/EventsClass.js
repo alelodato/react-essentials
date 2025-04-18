8795cb2cf0c871571bd59776d9f0ccd273ac33d5
alelodato <alss.ldt@gmail.com>
2025-04-18 09:33:52 +0200

    assigned event click handler to class and functional components

--- my-app/src/components/EventsClass.js
+++ my-app/src/components/EventsClass.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
 
 export class EventsClass extends Component {
+    clickHanlder() {
+        console.log("Clicked the class button")
+    }
   render() {
     return (
       <div>
-        <button>click me - class component</button>
+        <button onClick={this.clickHanlder}>click me - class component</button>
       </div>
     )
   }
